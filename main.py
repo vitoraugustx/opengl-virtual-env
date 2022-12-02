569ecf63540ad87b5a89ad58f7bd6f05919e035c
Vitor <vitor.arp2010@gmail.com>
2022-12-02 11:15:31 -0300

    Alterações na luz

--- main.py
+++ main.py
@@ -85,12 +85,43 @@ def frustum(left, right, bottom, top, near, far):
     
 def setZBuffer():
 	glEnable(GL_DEPTH_TEST)
-    
+
+def setLight():
+    # Iluminação
+    glEnable(GL_LIGHTING)
+
+    # Conjuto de luzes
+    glEnable(GL_LIGHT0)
+    glEnable(GL_LIGHT1)
+    glEnable(GL_LIGHT2)
+
+    # Propriedades da cor do material
+    glEnable(GL_COLOR_MATERIAL)
+    glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE)
+
+    # Luz 0
+    glLightfv(GL_LIGHT0, GL_AMBIENT, (.1, .1, .1, 1))
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, (.2, .2, .2, 1))
+    glLightfv(GL_LIGHT0, GL_SPECULAR, (.1, .1, .1, 1))
+    glLight(GL_LIGHT0, GL_POSITION,  (0, 5, 6, 1))
+
+    # Luz 4
+    glLightfv(GL_LIGHT1, GL_AMBIENT, (.1, .1, .1, 1))
+    glLightfv(GL_LIGHT1, GL_DIFFUSE, (.1, .1, .1, 1))
+    glLightfv(GL_LIGHT1, GL_SPECULAR, (.1, .1, .1, 1))
+    glLight(GL_LIGHT1, GL_POSITION,  (5, 0, 6, 1))
+
+    # Luz 5
+    glLightfv(GL_LIGHT2, GL_AMBIENT, (.1, .1, .1, 1))
+    glLightfv(GL_LIGHT2, GL_DIFFUSE, (.1, .1, .1, 1))
+    glLightfv(GL_LIGHT2, GL_SPECULAR, (.1, .1, .1, 1))
+    glLight(GL_LIGHT2, GL_POSITION,  (-5, 0, 6, 1))
+
 
 pygame.init()
-display = (700, 500)
+display = (800, 450)
 
-pygame.display.set_mode(display, pygame.DOUBLEBUF | pygame.OPENGL)
+pygame.display.set_mode(display, pygame.DOUBLEBUF | pygame.OPENGL | pygame.RESIZABLE)
 clock = pygame.time.Clock()
 
 gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
@@ -98,54 +129,60 @@ setZBuffer()
 glTranslatef(0.0,0.0, -15)
 scale(rate=0.5)
 
-# Iluminação
-glEnable(GL_LIGHTING)
-glEnable(GL_LIGHT0)
-glEnable(GL_COLOR_MATERIAL)
-glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE)
-glLightfv(GL_LIGHT0, GL_AMBIENT, (0, 0, 0, 1))
-glLightfv(GL_LIGHT0, GL_DIFFUSE, (1, 1, 1, 1))
-glLight(GL_LIGHT0, GL_POSITION,  (2, 2, 2, 1))
-
+setLight()
 
 model = OBJ('src/blender_objs/logo_furg_3.obj')
 
 run = True
 while run:
-    clock.tick(100)
+    clock.tick(120)
+    keys = pygame.key.get_pressed()
     for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-            if event.type == pygame.MOUSEWHEEL:
-                if event.y > 0:
-                    scale(1.1)
-                else:
-                    scale(0.9)
-            if event.type == pygame.MOUSEMOTION:
-                if pygame.mouse.get_pressed()[0]:
-                    rotateXYZ(-event.rel[1], -event.rel[0], 0)
-                    
-                if pygame.mouse.get_pressed()[2]:
-                    translateXYZ(event.rel[0]/100, -event.rel[1]/100, 0)
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+        if event.type == pygame.MOUSEWHEEL:
+            if event.y > 0:
+                scale(1.1)
+            else:
+                scale(0.9)
+        if event.type == pygame.MOUSEMOTION:
+            if pygame.mouse.get_pressed()[0]:
+                rotateXYZ(-event.rel[1], -event.rel[0], 0)
+                
+            if pygame.mouse.get_pressed()[2]:
+                translateXYZ(event.rel[0]/100, -event.rel[1]/100, 0)
 
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_o:
-                    glLoadIdentity()
-                    ortho(-1, 1, -1, 1, -1, 1)
-                    scale(0.06)
+        if keys[pygame.K_w]:
+            translateXYZ(0, 0, 0.1)
 
-                if event.key == pygame.K_r:
-                    glLoadIdentity()
-                    gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
-                    glTranslatef(0.0,0.0, -15)
-                    scale(rate=0.5)
+        if keys[pygame.K_s]:
+            translateXYZ(0, 0, -0.1)
 
-                if event.key == pygame.K_p:
-                    frustum(-1, 1, -1, 1, 1, 100)         
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_o:
+                glLoadIdentity()
+                ortho(-1, 1, -1, 1, -1, 1)
+                scale(0.05)
+                setLight()
+
+                
+            if event.key == pygame.K_r:
+                glLoadIdentity()
+                gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
+                glTranslatef(0.0,0.0, -15)
+                scale(rate=0.5)
+                setLight()
+
+            if event.key == pygame.K_p:
+                frustum(-1, 1, -1, 1, 1, 100)  
+                setLight()
+                  
                     
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glClearColor(0.31, 0.31, 0.31, 0.5)
     
     glPushMatrix()
     
